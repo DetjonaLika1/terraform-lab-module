0b2a79bff926bd4ea6b4c3c98c7fae46e42b8913
Detjona <detjona.lika@student.uni-pr.edu>
2022-10-27 21:22:15 +0200

    second

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
 
+module "user-Detjona-module" {
+  source  = "app.terraform.io/devOpsKursi/user-milot-module/iam"
+  version = "2.0.2"
+  # insert required variables here
+  user_name   = "Detjona-user"
+  policy_name = "Detjona-policy"
+  policy_document = {
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*", "iam:GetAccountPasswordPolicy", "elasticloadbalancing:DescribeLoadBalancers"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "iam:ChangePassword"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  }
+}
